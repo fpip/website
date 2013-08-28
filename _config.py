5ae3d74675e229c34a309dc43ed68de6b9b1b3e4
Mike Pirnat <mpirnat@gmail.com>
2013-08-27 23:47:17 -0400

    Temporary shift to 'new' domain

--- _config.py
+++ _config.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-site.url = "http://www.frompythonimportpodcast.com"
+#site.url = "http://www.frompythonimportpodcast.com"
+site.url = "http://new.frompythonimportpodcast.com"
 site.cdn_url = "http://cdn.frompythonimportpodcast.com"
 
 blog = controllers.blog
